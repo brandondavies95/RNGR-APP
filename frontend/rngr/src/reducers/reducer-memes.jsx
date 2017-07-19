ea1ffdf13a7c90512d4f8e56c3010e1d3dfb401c
Brandon Davies <bdavies@CIT-SCC112-1113.cs.dixie.edu>
2017-07-19 13:55:13 -0600

    Updated reducer-memes with all sounds and restructured

--- frontend/rngr/src/reducers/reducer-memes.jsx
+++ frontend/rngr/src/reducers/reducer-memes.jsx
@@ -25,5 +25,50 @@ export default function () {
       url: '../assets/audio/sadViolin.wav',
       id: 4,
     },
+    {
+      title: 'Fun To Do Bad Things',
+      url: '../assets/audio/badThings.wav',
+      id: 5,
+    },
+    {
+      title: 'Hoodrat Stuff With My Friends',
+      url: '../assets/audio/hoodRatStuff.wav',
+      id: 6,
+    },
+    {
+      title: 'I Like Turtles',
+      url: '../assets/audio/iliketurtles.wav',
+      id: 7,
+    },
+    {
+      title: 'Is This Real Life',
+      url: '../assets/audio/isthisreallife.wav',
+      id: 8,
+    },
+    {
+      title: 'Leeroy Jenkins',
+      url: '../assets/audio/leeroyjenkins.wav',
+      id: 9,
+    },
+    {
+      title: 'Ohhhhh',
+      url: '../assets/audio/ohhhhh.wav',
+      id: 10,
+    },
+    {
+      title: 'This Is Sparta',
+      url: '../assets/audio/thisissparta.wav',
+      id: 11,
+    },
+    {
+      title: 'Weather Boy',
+      url: '../assets/audio/weatherBoy.wav',
+      id: 12,
+    },
+    {
+      title: 'I Yanked The Thing',
+      url: '../assets/audio/yankedTheThing.wav',
+      id: 13,
+    },
   ];
 }
